d6a973a575139b27302a63c1f30555d6617d0a66
Sergiy Popovych <sergiy.popovich@gmail.com>
2021-04-19 11:41:42 -0700

    scriber

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="corgie",
-    version="0.0.9",
+    version="0.1.0",
     author="Sergiy Popovych",
     author_email="sergiy.popovich@gmail.com",
     description="Connectomics Registration General Inference Engine",
