a4ea4b664c5dddf82c6ddf104ae349ad2882dbe8
Thomas Macrina <tmacrina@princeton.edu>
2021-06-03 13:15:19 +0000

    Remove chunk_z arg if dst spec is specified for downsample

--- corgie/cli/downsample.py
+++ corgie/cli/downsample.py
@@ -160,7 +160,6 @@ def downsample(
             caller_name="dst_layer layer",
             readonly=False,
             reference=src_layer,
-            chunk_z=chunk_z,
             overwrite=True,
         )
     bcube = get_bcube_from_coords(start_coord, end_coord, coord_mip)
