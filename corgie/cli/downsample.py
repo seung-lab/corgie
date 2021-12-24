bb55a9c4b846ce949fa2330eb5d5c594a89f04d5
Sergiy Popovych <sergiy.popovich@gmail.com>
2021-12-24 07:18:02 -0800

    m

--- corgie/cli/downsample.py
+++ corgie/cli/downsample.py
@@ -1,4 +1,5 @@
 import click
+import torch
 
 from corgie import scheduling
 from corgie.log import logger as corgie_logger
@@ -25,6 +26,7 @@ class DownsampleJob(scheduling.Job):
         chunk_z,
         mips_per_task=3,
         dst_layer=None,
+        preserve_zeros=False
     ):
         if dst_layer is None:
             dst_layer = src_layer
@@ -36,6 +38,7 @@ class DownsampleJob(scheduling.Job):
         self.chunk_xy = chunk_xy
         self.chunk_z = chunk_z
         self.mips_per_task = mips_per_task
+        self.preserve_zeros = preserve_zeros
 
         super().__init__()
 
@@ -58,6 +61,7 @@ class DownsampleJob(scheduling.Job):
                     this_mip_start,
                     this_mip_end,
                     input_chunk,
+                    preserve_zeros=self.preserve_zeros
                 )
                 for input_chunk in chunks
             )
@@ -78,13 +82,14 @@ class DownsampleJob(scheduling.Job):
 
 
 class DownsampleTask(scheduling.Task):
-    def __init__(self, src_layer, dst_layer, mip_start, mip_end, bcube):
+    def __init__(self, src_layer, dst_layer, mip_start, mip_end, bcube, preserve_zeros):
         super().__init__()
         self.src_layer = src_layer
         self.dst_layer = dst_layer
         self.mip_start = mip_start
         self.mip_end = mip_end
         self.bcube = bcube
+        self.preserve_zeros = preserve_zeros
 
     def execute(self):
         src_data = self.src_layer.read(bcube=self.bcube, mip=self.mip_start)
@@ -93,6 +98,10 @@ class DownsampleTask(scheduling.Task):
         downsampler = self.src_layer.get_downsampler()
         for mip in range(self.mip_start, self.mip_end):
             dst_data = downsampler(src_data)
+            if self.preserve_zeros:
+                zeros = src_data == 0
+                zeros_d = torch.nn.functional.max_pool2d(zeros.float(), 2, 2)
+                dst_data[zeros_d > 0] = 0
             self.dst_layer.write(dst_data, bcube=self.bcube, mip=mip + 1)
             src_data = dst_data
 
@@ -123,6 +132,7 @@ class DownsampleTask(scheduling.Task):
 @corgie_option("--chunk_xy", "-c", nargs=1, type=int, default=2048)
 @corgie_option("--chunk_z", nargs=1, type=int, default=1)
 @corgie_option("--mips_per_task", nargs=1, type=int, default=3)
+@corgie_option("--preserve_zeros/--no_preserve_zeros", default=False)
 @corgie_optgroup("Data Region Specification")
 @corgie_option("--start_coord", nargs=1, type=str, required=True)
 @corgie_option("--end_coord", nargs=1, type=str, required=True)
@@ -140,6 +150,7 @@ def downsample(
     start_coord,
     end_coord,
     coord_mip,
+    preserve_zeros
 ):
     scheduler = ctx.obj["scheduler"]
     corgie_logger.debug("Setting up Source and Destination layers...")
@@ -172,6 +183,7 @@ def downsample(
         chunk_xy=chunk_xy,
         chunk_z=chunk_z,
         mips_per_task=mips_per_task,
+        preserve_zeros=preserve_zeros,
     )
 
     # create scheduler and execute the job
