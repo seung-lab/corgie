1ac7439447a67c9b60317fb8653ee26f381a11d9
Thomas Macrina <tmacrina@princeton.edu>
2022-02-09 14:15:28 +0000

    Remove unused vote functions

--- corgie/cli/vote.py
+++ corgie/cli/vote.py
@@ -12,213 +12,6 @@ from corgie.argparsers import (
 )
 
 
-def compute_softmin_temp(dist: float, weight: float, size: int):
-    """Compute softmin temp given binary assumptions
-
-    Assumes that voting subsets are either correct or incorrect.
-
-    Args:
-        dist: distance between the average differences of
-            a correct and incorrect subset.
-        weight: desired weight to be assigned for correct/incorrect
-            distance.
-        size: the number of subsets involved in voting. If the 
-            number of vectors involved in voting is n, and k is the smallest
-            number that represents a majority of n, then size should be 
-            (n choose k). 
-
-    Returns:
-        float for softmin temperature that will achieve this weight
-    """
-    assert (weight > 0) and (weight < 1)
-    assert size > 0
-    assert dist >= 0
-    return -dist / (log(1 - weight) - log(weight * size) + 1e-5)
-
-
-class VoteOverFieldsJob(scheduling.Job):
-    def __init__(
-        self,
-        input_fields,
-        output_field,
-        chunk_xy,
-        bcube,
-        mip,
-        softmin_temp=None,
-        blur_sigma=1.0,
-    ):
-        self.input_fields = input_fields
-        self.output_field = output_field
-        self.chunk_xy = chunk_xy
-        self.bcube = bcube
-        self.mip = mip
-        self.softmin_temp = softmin_temp
-        self.blur_sigma = blur_sigma
-        super().__init__()
-
-    def task_generator(self):
-        tmp_key = list(self.input_fields.keys())[0]
-        tmp_layer = self.input_fields[tmp_key]
-        chunks = tmp_layer.break_bcube_into_chunks(
-            bcube=self.bcube, chunk_xy=self.chunk_xy, chunk_z=1, mip=self.mip
-        )
-
-        tasks = [
-            VoteOverFieldsTask(
-                input_fields=self.input_fields,
-                output_field=self.output_field,
-                mip=self.mip,
-                bcube=chunk,
-                softmin_temp=self.softmin_temp,
-                blur_sigma=self.blur_sigma,
-            )
-            for chunk in chunks
-        ]
-
-        corgie_logger.debug(
-            "Yielding VoteTask for bcube: {}, MIP: {}".format(self.bcube, self.mip)
-        )
-        yield tasks
-
-
-class VoteOverFieldsTask(scheduling.Task):
-    def __init__(
-        self, input_fields, output_field, mip, bcube, softmin_temp, blur_sigma=1.0
-    ):
-        """Find median vector for single location over set of fields
-
-        Notes:
-            Does not ignore identity fields.
-            Padding & cropping is not necessary.
-
-        Args:
-            input_fields (Stack): collection of fields
-            output_field (Layer)
-            mip (int)
-            bcube (BoundingCube)
-        """
-        super().__init__()
-        self.input_fields = input_fields
-        self.output_field = output_field
-        self.mip = mip
-        self.bcube = bcube
-        if softmin_temp is None:
-            softmin_temp = compute_softmin_temp(
-                dist=1, weight=0.99, size=len(input_fields)
-            )
-        self.softmin_temp = softmin_temp
-        self.blur_sigma = blur_sigma
-
-    def execute(self):
-        bcube = self.bcube
-        mip = self.mip
-        fields = [f.read(bcube, mip=mip) for f in self.input_fields.values()]
-        fields = torch.cat([f for f in fields]).field()
-        voted_field = fields.vote(
-            softmin_temp=self.softmin_temp, blur_sigma=self.blur_sigma
-        )
-        self.output_field.write(data_tens=voted_field, bcube=bcube, mip=self.mip)
-
-
-class VoteOverZJob(scheduling.Job):
-    def __init__(
-        self,
-        input_field,
-        output_field,
-        chunk_xy,
-        bcube,
-        z_list,
-        mip,
-        softmin_temp=None,
-        blur_sigma=1.0,
-        weights_layer=None,
-    ):
-        self.input_field = input_field
-        self.output_field = output_field
-        self.chunk_xy = chunk_xy
-        self.bcube = bcube
-        self.z_list = z_list
-        self.mip = mip
-        self.softmin_temp = softmin_temp
-        self.blur_sigma = blur_sigma
-        self.weights_layer = weights_layer
-        super().__init__()
-
-    def task_generator(self):
-        chunks = self.output_field.break_bcube_into_chunks(
-            bcube=self.bcube, chunk_xy=self.chunk_xy, chunk_z=1, mip=self.mip
-        )
-
-        tasks = [
-            VoteOverZTask(
-                input_field=self.input_field,
-                output_field=self.output_field,
-                mip=self.mip,
-                bcube=chunk,
-                z_list=self.z_list,
-                softmin_temp=self.softmin_temp,
-                blur_sigma=self.blur_sigma,
-                weights_layer=self.weights_layer,
-            )
-            for chunk in chunks
-        ]
-
-        corgie_logger.debug(
-            "Yielding VoteOverZTask for bcube: {}, MIP: {}".format(self.bcube, self.mip)
-        )
-        yield tasks
-
-
-class VoteOverZTask(scheduling.Task):
-    def __init__(
-        self,
-        input_field,
-        output_field,
-        mip,
-        bcube,
-        z_list,
-        softmin_temp,
-        blur_sigma=1.0,
-        weights_layer=None,
-    ):
-        """Find median vector for set of locations in a single field
-
-        Notes:
-            Does not ignore identity fields.
-            Padding & cropping is not necessary.
-
-        Args:
-            input_field (Layer)
-            output_field (Layer)
-            mip (int)
-            bcube (BoundingCube)
-            z_list (range, [int])
-            weight_layer (Layer): if set, where intermediary weights will be written
-        """
-        super().__init__()
-        self.input_field = input_field
-        self.output_field = output_field
-        self.mip = mip
-        self.bcube = bcube
-        self.z_list = z_list
-        if softmin_temp is None:
-            softmin_temp = compute_softmin_temp(dist=1, weight=0.99, size=len(z_list))
-        self.softmin_temp = softmin_temp
-        self.blur_sigma = blur_sigma
-        self.weights_layer = weights_layer
-
-    def execute(self):
-        fields = []
-        for z in self.z_list:
-            bcube = self.bcube.reset_coords(zs=z, ze=z + 1, in_place=False)
-            fields.append(self.input_field.read(bcube, mip=self.mip))
-        fields = torch.cat([f for f in fields]).field()
-        voted_field = fields.vote(
-            softmin_temp=self.softmin_temp, blur_sigma=self.blur_sigma
-        )
-        self.output_field.write(data_tens=voted_field, bcube=self.bcube, mip=self.mip)
-
-
 class VoteJob(scheduling.Job):
     def __init__(
         self,
@@ -280,7 +73,7 @@ class VoteTask(scheduling.Task):
         blur_sigma=2.0,
         weights_layer=None,
     ):
-        """Find median vector for set of locations in a set of fields
+        """Find median-like field with highest priority from a set of fields
 
         Notes:
             Does not ignore identity fields.
@@ -292,6 +85,9 @@ class VoteTask(scheduling.Task):
             mip (int)
             bcube (BoundingCube)
             z_offsets (range, [int]): offsets from z where fields should be accessed
+            consensus_threshold (float): distance from winning subset that will still be 
+                considered consensus
+            blur_sigma (float): std of Gaussian that will blur weights of the fields
             weight_layer (Layer): if set, where intermediary weights will be written
         """
         super().__init__()
@@ -321,15 +117,10 @@ class VoteTask(scheduling.Task):
             priorities.append(torch.full_like(fields[-1][:,0,...], fill_value=len(self.input_fields) -priority))
         fields = torch.cat([f for f in fields]).field()
         priorities = torch.cat(priorities)
-        # weights = fields.get_vote_weights(
-        #     softmin_temp=self.softmin_temp, blur_sigma=self.blur_sigma
-        # )
+        weights = fields.get_priority_vote_weights(priorities=priorities, consensus_threshold=2)
         if self.weights_layer:
-            weights = fields.get_priority_vote_weights(priorities=priorities, consensus_threshold=2)
             self.weights_layer.write(data_tens=weights, bcube=self.bcube, mip=self.mip)
-            voted_field = (fields * weights.unsqueeze(-3)).sum(dim=0, keepdim=True)
-        else:
-            voted_field = fields.priority_vote(priorities=priorities, consensus_threshold=self.consensus_threshold, blur_sigma=self.blur_sigma)
+        voted_field = fields.smoothed_combination(weights=weights, blur_sigma=self.blur_sigma)
         self.output_field.write(data_tens=voted_field, bcube=self.bcube, mip=self.mip)
 
 
