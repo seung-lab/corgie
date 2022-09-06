36d921a2b4c0b21fe6a4ca3b36ccbb7a94008211
sergiy <sergiy@transistor-3.local>
2022-09-06 13:37:02 -0700

    clean up requirements

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
@@ -15,27 +15,28 @@ setuptools.setup(
     include_package_data=True,
     package_data={'': ['*.py']},
     install_requires=[
-      'sphinx-click',
+      'sphinx-click>=3',
       'torchfields',
-      'torch',
-      'gevent',
-      'torchvision',
-      'numpy',
-      'six',
-      'pyyaml',
+      'torch>=1.8',
+      'gevent>=21.1.2',
+      'torchvision>=0.9.1',
+      'numpy>=1.21',
+      'six>=1.16',
+      'pyyaml>=5.4.1',
       'mazepa>=0.1.1',
       'modelhouse>=0.1.2',
-      'click-option-group',
+      'click-option-group>=0.5',
       'click>=7,<8',
-      'procspec',
+      'procspec>=0.0.4',
+      'metroem>=0.1.1',
       'idna>=2.5',
       'google-auth>=1.11.0',
-      'cloud-volume',
-      'scikit-image',
-      'h5py',
-      'kimimaro',
+      'scikit-image>=0.18',
+      'h5py>=3.3.0',
+      'kimimaro>=2.1.0',
       'docutils<0.18',
-      'cloud-files',
+      'cloud-files>=2.0.1',
+      'cloud-volume>=5.0.0',
     ],
     entry_points={
         "console_scripts": [
